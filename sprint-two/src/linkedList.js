77f4e359ab17b3f0ffaba75cda67ac1ae81f3ca2
elchangtw <elchang.tw@gmail.com>
2019-10-23 20:28:36 -0700

    linked lists are easy

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,40 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    let newNode = Node(value);
+    if (list.head === null){
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    var temp = list.head.value;
+    if (list.head !== null) {
+      list.head = list.head.next;
+    }
+    return temp;
   };
 
   list.contains = function(target) {
+    var currNode = list.head;
+    var flag = false;
+
+    var search = function(currNode) {
+      if (currNode.value === target) {
+        flag = true;
+      }
+      if (currNode.next !== null) {
+        search(currNode.next);
+      }
+    };
+
+    search(currNode);
+    return flag;
+    
   };
 
   return list;

ae40ca293893cd7a0c9fe68eecebe8dff9b7ff71
elchangtw <elchang.tw@gmail.com>
2019-10-25 15:08:54 -0700

    complete hashTable; apply buckets as arrays

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,11 +3,35 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
-  this.otherStorage = LimitedArray(this._limit);
+  //this.otherStorage = LimitedArray(this._limit);
+};
+
+//var MAX_TUPLE_COUNT; //potentially to implement dynamic bucket size
+
+HashTable.prototype.findTuple = function(bucket, key) {
+  var result;
+  //debugger
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === key) {
+      result = i;
+      i = bucket.length; // exit for loop
+    }
+  }
+  return result;
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  var tuple = [k, v];
+  if (bucket === undefined) {
+    bucket = [];
+  }
+  if (this.findTuple(bucket, k) === undefined) {
+    bucket.push(tuple);
+  } else {
+    bucket[this.findTuple(bucket, k)][1] = v;
+  }
   // var obj = {};
   // var arr = [];
   // obj[k] = v;
@@ -15,32 +39,33 @@ HashTable.prototype.insert = function(k, v) {
   //   _.extend(obj, this._storage.get(index));
   // }
 
-  if (this._storage.get(index)) {
-    this.otherStorage.set(index, v);
-    return;
-  }
-
-  this._storage.set(index, v);
+  this._storage.set(index, bucket);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  if (this.otherStorage.get(index)) {
-    return this.otherStorage.get(index);
-  } else {
-    return this._storage.get(index);
-  }
   // if (this._storage.get(index)[k]) {
   //   return k;
   // } else {
   //   return false;
   // }
+  var bucket = this._storage.get(index);
+  var result;
+  if (this.findTuple(bucket, k) !== undefined) {
+    result = bucket[this.findTuple(bucket, k)][1];
+  }
+  return result;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-
-  this._storage.set(index, undefined);
+  var bucket = this._storage.get(index);
+  var result;
+  if (this.findTuple(bucket, k) !== undefined) {
+    result = bucket.splice(this.findTuple(bucket, k), 1);
+  }
+  this._storage.set(index, bucket);
+  return result;
 };
 
 

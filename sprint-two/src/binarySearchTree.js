5ba69b5cc833949f541d378718cd6d97766a7647
elchangtw <elchang.tw@gmail.com>
2019-10-25 16:03:02 -0700

    complete BST

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,22 +1,52 @@
 var BinarySearchTree = function(value) {
-  var obj = Object.create(methods);
-  obj.left = null;
-  obj.right = null;
-  return obj;
+  var newTree = Object.create(methods);
+  newTree.value = value;
+  newTree.left = null;
+  newTree.right = null;
+  return newTree;
 };
-
 var methods = {};
 
 methods.insert = function(value) {
-  var childNode = new BinarySearchTree();
+  var child = BinarySearchTree(value);
+  if (this.value > value) {
+    if (this.left === null) {
+      this.left = child;
+    } else {
+      this.left.insert(value);
+    }
+  } else {
+    if (this.right === null) {
+      this.right = child;
+    } else {
+      this.right.insert(value);
+    }
+  }
 };
 
 methods.contains = function(value) {
+  var result = false;
 
+  if (this.value === value) {
+    result = true;
+  } else {
+    if (this.value < value && this.right !== null) {
+      result = this.right.contains(value);
+    } else if (this.value > value && this.left !== null) {
+      result = this.left.contains(value);
+    }
+  }
+  return result;
 };
 
 methods.depthFirstLog = function(callback) {
-
+  callback(this.value);
+  if (this.left !== null) {
+    this.left.depthFirstLog(callback);
+  }
+  if (this.right !== null) {
+    this.right.depthFirstLog(callback);
+  }
 };
 
 

b464554ec153c6536758777c694445b079a06aa8
elchangtw <elchang.tw@gmail.com>
2019-10-24 21:01:04 -0700

    collision object error

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,18 +3,44 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this.otherStorage = LimitedArray(this._limit);
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // var obj = {};
+  // var arr = [];
+  // obj[k] = v;
+  // if (this._storage.get(index)) {
+  //   _.extend(obj, this._storage.get(index));
+  // }
+
+  if (this._storage.get(index)) {
+    this.otherStorage.set(index, v);
+    return;
+  }
+
+  this._storage.set(index, v);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this.otherStorage.get(index)) {
+    return this.otherStorage.get(index);
+  } else {
+    return this._storage.get(index);
+  }
+  // if (this._storage.get(index)[k]) {
+  //   return k;
+  // } else {
+  //   return false;
+  // }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  this._storage.set(index, undefined);
 };
 
 

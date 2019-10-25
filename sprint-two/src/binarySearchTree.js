f20fe21c0a5eb8380a2dce31343d13c26981f1a2
elchangtw <elchang.tw@gmail.com>
2019-10-24 21:19:25 -0700

    started BST

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,22 @@
 var BinarySearchTree = function(value) {
+  var obj = Object.create(methods);
+  obj.left = null;
+  obj.right = null;
+  return obj;
+};
+
+var methods = {};
+
+methods.insert = function(value) {
+  var childNode = new BinarySearchTree();
+};
+
+methods.contains = function(value) {
+
+};
+
+methods.depthFirstLog = function(callback) {
+
 };
 
 

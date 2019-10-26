2ce370376d333f7248f9c98ffe413da42dc2f401
elchangtw <elchang.tw@gmail.com>
2019-10-25 20:15:20 -0700

    hashtable should double in size when needed

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,6 +3,7 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._usage = 0;
   //this.otherStorage = LimitedArray(this._limit);
 };
 
@@ -10,7 +11,6 @@ var HashTable = function() {
 
 HashTable.prototype.findTuple = function(bucket, key) {
   var result;
-  //debugger
   for (var i = 0; i < bucket.length; i++) {
     if (bucket[i][0] === key) {
       result = i;
@@ -26,11 +26,37 @@ HashTable.prototype.insert = function(k, v) {
   var tuple = [k, v];
   if (bucket === undefined) {
     bucket = [];
+    this._usage++;
   }
+
   if (this.findTuple(bucket, k) === undefined) {
     bucket.push(tuple);
   } else {
     bucket[this.findTuple(bucket, k)][1] = v;
+  }
+
+  this._storage.set(index, bucket);
+  if ((this._usage / this._limit) > .6) {
+    var newHash = [];
+    for (var i = 0; i < this._limit; i++) {
+      // get our current storage and place into new one
+      newHash.push(this._storage.get(i)) === undefined ? newHash.push([]) : newHash.push(this._storage.get(i));
+    }
+    this._storage = LimitedArray(this._limit * 2);
+    this._limit = this._limit * 2;
+    for (var i = 0; i < newHash.length; i++) {
+      if (newHash[i] !== undefined && newHash[i].length > 0) {
+        for (var j = 0; j < newHash[i].length; j++) {
+          var index = getIndexBelowMaxForKey(newHash[i][j][0], this._limit);
+          var bucket = this._storage.get(index);
+          if (bucket === undefined) {
+            bucket = [];
+          }
+          bucket.push(newHash[i][j]);
+          this._storage.set(index, bucket);
+        }
+      }
+    }
   }
   // var obj = {};
   // var arr = [];
@@ -39,7 +65,6 @@ HashTable.prototype.insert = function(k, v) {
   //   _.extend(obj, this._storage.get(index));
   // }
 
-  this._storage.set(index, bucket);
 };
 
 HashTable.prototype.retrieve = function(k) {
@@ -63,6 +88,9 @@ HashTable.prototype.remove = function(k) {
   var result;
   if (this.findTuple(bucket, k) !== undefined) {
     result = bucket.splice(this.findTuple(bucket, k), 1);
+    if (bucket.length === 0) {
+      this._usage--;
+    }
   }
   this._storage.set(index, bucket);
   return result;
